9a9fbd22b1ecf1ee55668440907846018c37dceb
Quinn Zipse <qzipse@outlook.com>
2020-10-12 19:47:24 -0500

    Added login and registration

---
+++ login.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+<main class="h-100">
+    <div class="d-flex h-100">
+        <div class="row m-auto w-75">
+            <div class="col-lg-6">
+
+            </div>
+            <div class="col-lg-6 border-left">
+                <div class="p-4">
+                    <h2 class="lead" style="font-size: 2rem">Login</h2>
+                    <form class="mt-4">
+                        <label class="form-label" for="username_input">Email</label>
+                        <input type="text" autocomplete="username" name="username" id="username_input"
+                               class="form-control">
+                        <label class="form-label mt-2" for="password_input">Password</label>
+                        <input type="password" autocomplete="password" name="password" id="password_input"
+                               class="form-control">
+                        <button class="btn btn-primary mt-3">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+<main class="h-100 bg-light">
+    <div class="d-flex h-100">
+        <div class="m-auto w-75">
+            <div class="row">
+                <div class="offset-lg-3 col-lg-6">
+                    <div class="border rounded bg-white">
+                        <div class="p-4">
+                            <h2 class="lead" style="font-size:2rem">Register</h2>
+                            <form class="mt-4">
+                                <label class="form-label">First Name</label>
+                                <input type="text" name="firstName" id="firstNameInput">
+                                <label class="form-label" for="username_input">Email</label>
+                                <input type="text" name="username" id="username_input"
+                                       class="form-control">
+                                <label class="form-label mt-2" for="password_input">Password</label>
+                                <input type="password" name="password" id="password_input"
+                                       class="form-control">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+</body>
+</html>
