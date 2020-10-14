985b560cb5580aaa12140ae62621029ce0f1f5b5
Quinn Zipse <qzipse@outlook.com>
2020-10-13 19:00:35 -0500

    Finished Front End for login and register

--- login.php
+++ login.php
@@ -8,19 +8,19 @@
 <main class="h-100">
     <div class="d-flex h-100">
         <div class="row m-auto w-75">
-            <div class="col-lg-6">
+            <div class="col-lg-7">
 
             </div>
-            <div class="col-lg-6 border-left">
+            <div class="offset-lg-7 offset-md-5 col-md-7 col-lg-5 border-left">
                 <div class="p-4">
                     <h2 class="lead" style="font-size: 2rem">Login</h2>
                     <form class="mt-4">
                         <label class="form-label" for="username_input">Email</label>
-                        <input type="text" autocomplete="username" name="username" id="username_input"
-                               class="form-control">
+                        <input type="email" autocomplete="username" name="username" id="username_input"
+                               class="form-control" required>
                         <label class="form-label mt-2" for="password_input">Password</label>
                         <input type="password" autocomplete="password" name="password" id="password_input"
-                               class="form-control">
+                               class="form-control" required>
                         <button class="btn btn-primary mt-3">Login</button>
                     </form>
                 </div>
--- register.php
+++ register.php
@@ -10,18 +10,34 @@
         <div class="m-auto w-75">
             <div class="row">
                 <div class="offset-lg-3 col-lg-6">
-                    <div class="border rounded bg-white">
+                    <div class="border rounded bg-white shadow">
                         <div class="p-4">
                             <h2 class="lead" style="font-size:2rem">Register</h2>
+                            <hr>
                             <form class="mt-4">
-                                <label class="form-label">First Name</label>
-                                <input type="text" name="firstName" id="firstNameInput">
-                                <label class="form-label" for="username_input">Email</label>
-                                <input type="text" name="username" id="username_input"
-                                       class="form-control">
-                                <label class="form-label mt-2" for="password_input">Password</label>
-                                <input type="password" name="password" id="password_input"
-                                       class="form-control">
+                                <div class="row">
+                                    <div class="col-6 form-group">
+                                        <label class="form-label" for="firstNameInput">First Name</label>
+                                        <input type="text" name="firstName" id="firstNameInput" class="form-control"
+                                               required>
+                                    </div>
+                                    <div class="col-6 form-group">
+                                        <label class="form-label" for="lastNameInput">Last Name</label>
+                                        <input type="text" name="lastName" id="lastNameInput" class="form-control"
+                                               required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-label" for="username_input">Email</label>
+                                    <input type="email" name="username" id="username_input" required
+                                           class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-label" for="password_input">Password</label>
+                                    <input type="password" name="password" id="password_input" required
+                                           class="form-control">
+                                </div>
+                                <button class="btn btn-outline-primary mt-2">Register</button>
                             </form>
                         </div>
                     </div>
