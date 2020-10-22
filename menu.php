c789bc976ca0ac8442a33dac67f56b77abe99d0e
Ben Boehlke <58837035+Ben-Boehlke@users.noreply.github.com>
2020-10-22 18:53:54 -0500

    Testing membership checking

--- menu.php
+++ menu.php
@@ -1,9 +1,12 @@
 <?php
 $host = ($_SERVER['HTTPS'] ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'];
-require_once  './service/Auth.php';
+require_once  ($_SERVER["DOCUMENT_ROOT"] . '/service/Auth.php');
 $auth = new Auth();
 $user = $auth->isLoggedIn();
 ?>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
       integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
@@ -15,17 +18,12 @@ $user = $auth->isLoggedIn();
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-            <?php $host = ($_SERVER['HTTPS'] ? 'https://' : 'http://') . $_SERVER['HTTP_HOST']; ?>
-            <a class='nav-link' href='/index.php'>Home</a>
-            <a class='nav-link' href='/program/index.php'>Programs</a>
-            <a class='nav-link' href='/staff/index.php'>Staff</a>
-            <a class='nav-link' href='/login.php'>Login</a>
             <?php
                 echo "  <a class='nav-link' href='$host/index.php'>Home</a>
                         <a class='nav-link' href='$host/program/index.php'>Programs</a>";
                         if ($user) {
                             if ($user->MembershipStatus == 3) {
-                                echo "<a class='nav-link' href='$host/staff/'>Staff</a>";
+                                echo "<a class='nav-link' href='$host/staff/index.php'>Staff</a>";
                             }  
                             echo "<a class='nav-link' href='$host/logout.php'>Logout</a>";
                         } else {
