def8ea5aa5031669ed38915ed118c1b78f8d2fdc
Ben Boehlke <58837035+Ben-Boehlke@users.noreply.github.com>
2020-10-22 15:39:27 -0500

    Fixed menu.php syntax

--- menu.php
+++ menu.php
@@ -1,50 +1,42 @@
-<html lang="en">
-<head>
-    <title>Index</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <style>
-        
-        .navigBar {
-            width: 75%;
-            margin: auto;
-            background-color: #177CFF;
-            overflow: hidden;
-        }
-        
-        .navigBar a {
-            float: left;
-            color: #f2f2f2;
-            text-align: center;
-            padding: 12px 14px;
-            text-decoration: none;
-            font-size: 24px;
-            font-weight: bold;
-        }
-        
-        .navigBar a:hover {
-            background-color: #ddd;
-            color: black;
-        }
-        
-        .navigBar a.active {
-            background-color: #4CAF50;
-            color: white;
-        }
-        .navigBar a.YMCA {
-            font-size: 40px;
-            padding: 0px 35px;
-            background-color: black;
-            color: white;
-        }
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+      integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<style>
 
-        .navigBar a.log {
-            float: right;
-        }
+    .navigBar {
+        width: 75%;
+        margin: auto;
+        background-color: #177CFF;
+        overflow: hidden;
+    }
 
-    </style>
-</head>
-<body>
+    .navigBar a {
+        float: left;
+        color: #f2f2f2;
+        text-align: center;
+        padding: 12px 14px;
+        text-decoration: none;
+        font-size: 24px;
+        font-weight: bold;
+    }
+
+    .navigBar a:hover {
+        background-color: #ddd;
+        color: black;
+    }
+
+    .navigBar a.YMCA {
+        font-size: 40px;
+        padding: 0 35px;
+        background-color: black;
+        color: white;
+    }
+
+    .navigBar a.log {
+        float: right;
+    }
+
+</style>
+
 <div class="navigBar">
     <a class="YMCA">YMCA</a>
     <a href="index.php">Home</a>
@@ -53,7 +45,3 @@
     <a class="log" href="login.php">Login</a>
 </div>
 
-</body>
-
-
-</html>
