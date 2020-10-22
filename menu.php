e355f4c4d59369dd9c5c2f8c5f26dd28d1097cb7
Ben Boehlke <58837035+Ben-Boehlke@users.noreply.github.com>
2020-10-22 16:42:06 -0500

    Added additional web page links on nav bar

--- menu.php
+++ menu.php
@@ -1,18 +1,17 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
       integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #47b3ff;">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0851c7;">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-            <?php $host = ($_SERVER['HTTPS'] ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'];
-            echo "<a class='nav-link' href='$host/index.php'>Home</a>";
-            echo '<a class="nav-link" href="http://' . $host . '/program/index.php">Programs</a>';
-            echo '<!--<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->';
-            echo '<a class="nav-link" href="login.php">Login</a>';
-            ?>
+            <?php $host = ($_SERVER['HTTPS'] ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'];?>
+                <a class='nav-link' href='$host/index.php'>Home</a>
+                <a class='nav-link' href='$host/program/index.php'>Programs</a>
+                <a class='nav-link' href='$host/staff/index.php'>Staff</a>
+                <a class='nav-link' href='$host/login.php'>Login</a>
         </div>
     </div>
 </nav>
