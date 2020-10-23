1758542992c614617ede99e2d576e13a8737f1b5
Quinn Zipse <qzipse@outlook.com>
2020-10-22 21:48:42 -0500

    Done with the program adder! :)

---
+++ service/addProgram.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'Auth.php';
+require_once 'MySQLConnection.php';
+
+// Verify that they are indeed a staff member.
+$auth = new Auth();
+$user = $auth->authorizeStaff();
+
+if (!$user) {
+    http_send_status(400);
+    exit(400);
+}
+
+$mysql = new MySQLConnection();
+
+$program_name = mysqli_real_escape_string($mysql->conn, $_REQUEST['name']);
+$location = mysqli_real_escape_string($mysql->conn, $_REQUEST['location']);
+$capacity = mysqli_real_escape_string($mysql->conn, $_REQUEST['capacity']);
+$sdate = mysqli_real_escape_string($mysql->conn, $_REQUEST['start_date']);
+$edate = mysqli_real_escape_string($mysql->conn, $_REQUEST['end_date']);
+$stime = mysqli_real_escape_string($mysql->conn, $_REQUEST['start_time']);
+$etime = mysqli_real_escape_string($mysql->conn, $_REQUEST['end_time']);
+$dow = mysqli_real_escape_string($mysql->conn, $_REQUEST['day_of_week']);
+$mem_price = mysqli_real_escape_string($mysql->conn, $_REQUEST['mem_price']);
+$non_mem_price = mysqli_real_escape_string($mysql->conn, $_REQUEST['non_mem_price']);
+$description = mysqli_real_escape_string($mysql->conn, ($_REQUEST['description'] ?? ''));
+
+//var_dump($program_name, $location, $capacity, $sdate, $stime, $mem_price, $non_mem_price, $description);
+
+$sql = "INSERT INTO Programs (Name, DescFile, Capacity, MemberFee, NonMemberFee, Location, start_date, end_date, start_time, end_time, day_of_week, ShortDesc)" .
+    " VALUES ('$program_name', '/tmp/null', '$capacity', '$mem_price', '$non_mem_price', '$location', '$sdate', '$edate', '$stime', '$etime', '$dow', '$description');";
+$result = mysqli_query($mysql->conn, $sql);
+
+if ($result) {
+    header("Location: /staff/?programCreated=1");
+    http_send_status(200);
+    exit(200);
+}
+header("Location: /staff/addProgram.php?failed=1");
+http_send_status(400);
+exit(400);
