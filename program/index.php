a9ec9ebb27c4425777a60517946fb5e57ebaf8f3
Ben Boehlke <58837035+Ben-Boehlke@users.noreply.github.com>
2020-10-22 20:27:34 -0500

    Program Registration page basic format complete.

--- program/index.php
+++ program/index.php
@@ -9,15 +9,16 @@
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">Weight Lifting</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Description</p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Cras justo odio</li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Vestibulum at eros</li>
+                    <li class="list-group-item">Where</li>
+                    <li class="list-group-item">When</li>
+                    <li class="list-group-item">Experience Level</li>
+                    <li class="list-group-item">Fee</li>
                 </ul>
                 <div class="card-body">
-                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Register For Class</a>
 
                 </div>
             </div>
