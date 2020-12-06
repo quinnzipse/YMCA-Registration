69c069bb0be6ca2bd5c75b50cf7269f765e28223
root <root@git.jordan.waughtal.rocks>
2020-12-06 18:15:55 +0000

    Images and Menu

--- menu.php
+++ menu.php
@@ -9,7 +9,7 @@ $user = $auth->isLoggedIn();
 ?>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
       integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
@@ -26,7 +26,7 @@ $user = $auth->isLoggedIn();
                         <a class='nav-link' href='$host/program/'>Programs</a>";
                         if ($user) {
                             if ($user->MembershipStatus == 3) {
-                                echo "<a class='nav-link' href='$host/staff/'>Staff</a>";
+                                echo "<a class='nav-link' href='$host/admin/'>Admin</a>";
                             } 
                             echo "<a class='nav-link' href='$host/accountinfo.php'>$user->FirstName</a>";
                             echo "<a class='nav-link' href='$host/logout.php'>Logout</a>";
