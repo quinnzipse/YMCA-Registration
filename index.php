d9319e1e9d0cb44f0e2fc5b3520e88e217e39df8
Ben Boehlke <58837035+Ben-Boehlke@users.noreply.github.com>
2020-12-08 16:04:35 -0600

    fixed program registration table

--- index.php
+++ index.php
@@ -43,6 +43,31 @@ if (isset($_REQUEST['loggedOut'])) {
     ";
 }
 
+function print_program($program) {
+    $sdate = $program->startDate->format("M, d Y");
+    $edate = $program->endDate->format("M, d Y");
+    $stime = $program->startTime->format('g:i A');
+    $etime = $program->endTime->format('g:i A');
+    $days_of_week = join("'s, ", $program->getDaysOfWeek());
+    echo "<div class='col-lg-4 col-md-6 mt-3 '>";
+    echo "\t<div class='card border-dark h-100' style='border-radius: 20px'>";
+    echo "\t\t<div class='card-body'>";
+    echo "\t\t\t<h5 class='card-title'>$program->name</h5>";
+    echo "\t\t\t<p class='card-text'>$program->shortDesc</p>";
+    echo "\t\t</div>";
+    echo "\t\t<ul class='list-group list-group-flush'>";
+    echo "\t\t\t<li class='list-group-item'>$program->location</li>";
+    echo "\t\t\t<li class='list-group-item'>";
+    echo "\t\t\t\t<p>$sdate to $edate</p>";
+    echo "\t\t\t\t<p> $days_of_week's, $stime - $etime</p>";
+    echo "\t\t\t</li>";
+    echo "\t\t\t<li class='list-group-item'><p>Member Fee:   $$program->memberFee</p> <p>Non Member Fee:   $$program->nonMemberFee</p></li>";
+    echo "\t\t</ul>";
+    echo "\t\t<a href='#' class='btn btn-block' style='background-color: red; color: white '>Cancel Registration</a>";
+    echo "\t</div>";
+    echo "</div>";
+}
+
 ?>
 <!--<h2 class="display-4 text-center mt-5">Good Morning--><?php //echo ($GLOBALS['user'] ? ', ' . $GLOBALS['user']->FirstName : '') ?><!--!</h2>-->
 
@@ -73,10 +98,12 @@ if (isset($_REQUEST['loggedOut'])) {
 <div class="container">
     <div class="row mt-3">
         <?php
-        $progs = Program::getParticipantProgram($loggedIn->ID);
-        foreach ($progs as $obj) {
-            print_program($obj);
-        }
+            if ($loggedIn) {
+                $progs = Program::getParticipantProgram($loggedIn->ID);
+                foreach ($progs as $obj) {
+                    print_program($obj);
+                }
+            }
         ?>
     </div>
 </div>
