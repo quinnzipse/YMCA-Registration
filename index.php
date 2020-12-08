fa0cb5697710ab3bdaed2f62342230b21867d77c
Ben Boehlke <58837035+Ben-Boehlke@users.noreply.github.com>
2020-12-08 15:42:49 -0600

    resized slide show, added code for listing what programs they are registered for. welcome message will only show when just logged in, just logged out, or just got the the site.

--- index.php
+++ index.php
@@ -2,10 +2,51 @@
 <head>
     <title>YMCA Home</title>
 </head>
-<?php include 'menu.php'; ?>
+<?php
+require 'models/Program.php';
+include 'menu.php';
+$loggedIn = $auth->isLoggedIn();
+$userProgs = array();
+
+if (isset($_REQUEST['loggedOut'])) {
+    echo "
+    <script>
+        Swal.fire({
+            title: 'Welcome!',
+            html: 'You are not logged in and cannot register for programs right now.  Would you like to log in?',
+            showConfirmButton: true,
+            confirmButtonText: 'Log In!',
+            showDenyButton: true,
+            denyButtonText: 'Not Now'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.replace('login.php');
+            }
+        })
+    </script>
+    ";
+} else if (isset($_REQUEST['loggedIn'])) {
+    echo "
+    <script>
+        Swal.fire({
+            title: 'Welcome $loggedIn->FirstName!',
+            showConfirmButton: true,
+            confirmButtonText: 'Browse Classes!',
+            showDenyButton: true,
+            denyButtonText: 'Not Now'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.replace('/program/index.php');
+            }
+        })
+    </script>
+    ";
+}
+
+?>
 <!--<h2 class="display-4 text-center mt-5">Good Morning--><?php //echo ($GLOBALS['user'] ? ', ' . $GLOBALS['user']->FirstName : '') ?><!--!</h2>-->
 
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="width: 45vw; margin-left: auto; margin-right: auto; margin-top: 5vh; max-height: 80vh">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="width: 72vw; margin-left: auto; margin-right: auto; margin-top: 5vh; max-height: 128vh">
     <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -26,20 +67,21 @@
             <img src="../img\yoga.jpg" class="d-block w-100" alt="...">
         </div>
     </div>
+
 </div>
-<script>
-    Swal.fire({
-        title: 'Welcome!',
-        html: 'You are not logged in and cannot register for programs right now.  Would you like to log in?',
-        showConfirmButton: true,
-        confirmButtonText: 'Log In!',
-        showDenyButton: true,
-        denyButtonText: 'Not Now'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.replace("login.php");
+
+<div class="container">
+    <div class="row mt-3">
+        <?php
+        $progs = Program::getParticipantProgram($loggedIn->ID);
+        foreach ($progs as $obj) {
+            print_program($obj);
         }
-    })
-</script>
+        ?>
+    </div>
+</div>
+
+
+
 
 </html>
