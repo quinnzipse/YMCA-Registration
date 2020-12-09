ab8e04f42f0a8dd87a8b01a2b8623a9cf8fe39b3
Quinn Zipse <qzipse@outlook.com>
2020-12-08 19:55:19 -0600

    Finished Program Searching

--- program/index.php
+++ program/index.php
@@ -1,8 +1,7 @@
 <?php
 require "models/Program.php";
 include "menu.php";
-$page = $_GET['page'] ?? 0;
-$progs = Program::getPrograms($page);
+$progs = Program::search($_GET['s'] ?? '');
 $loggedIn = $auth->isLoggedIn();
 $userProgs = array();
 if ($loggedIn) $userProgs = Program::getParticipantProgram($loggedIn->userID);
@@ -49,7 +48,34 @@ function print_program($program, bool $disabled, bool $registered)
     <title>Browse Programs</title>
 </head>
 <div class="container">
-    <div class="row mt-3">
+    <div class="row mt-5">
+        <div class="col-md-6">
+            <h2>Browse Programs</h2>
+        </div>
+        <form class="col-md-6 mb-0 mt-2">
+            <label for="search" class="sr-only">Search</label>
+            <div class="input-group-sm input-group">
+                <input type="text" id="search" name="s" class="form-control"
+                       value="<?php echo $_REQUEST['s'] ?? ''; ?>"
+                       placeholder="Search...">
+                <div class="input-group-append">
+                    <button class="btn btn-sm btn-primary">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search"
+                             fill="currentColor"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+                            <path fill-rule="evenodd"
+                                  d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <hr class="mt-0">
+
+    <div class="row">
         <?php
         foreach ($progs as $obj) {
             $disable = false;
