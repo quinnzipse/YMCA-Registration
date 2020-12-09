7dfe0ab701c79124e67ea7e341c8c770922ac01a
Quinn Zipse <qzipse@outlook.com>
2020-12-09 02:05:21 -0600

    Fixed bug on account info page

--- accountinfo.php
+++ accountinfo.php
@@ -1,82 +1,84 @@
-<?php 
-    require "models/Program.php";
-	include 'menu.php';
-	if ($user == null):
-		header("Location: $host/login.php");
-	endif;
+<?php
+require "models/Program.php";
+include 'menu.php';
+if ($user == null):
+    header("Location: $host/login.php");
+endif;
 
-	function print_program($program) {
-            $sdate = $program->startDate->format("M, d Y");
-            $edate = $program->endDate->format("M, d Y");
-            $stime = $program->startTime->format('g:i A');
-            $etime = $program->endTime->format('g:i A');
-            $days_of_week = join("'s, ", $program->getDaysOfWeek());
-            echo "<div class='col-lg-4 col-md-6 mt-3 '>";
-            echo "\t<div class='card border-dark h-100' style='border-radius: 20px'>";
-			echo "\t\t<div class='card-body'>";
-			echo "\t\t\t<h5 class='card-title'>$program->name</h5>";
-			echo "\t\t\t<p class='card-text'>$program->shortDesc</p>";
-			echo "\t\t</div>";
-			echo "\t\t<ul class='list-group list-group-flush'>";
-			echo "\t\t\t<li class='list-group-item'>$program->location</li>";
-			echo "\t\t\t<li class='list-group-item'>";
-			echo "\t\t\t\t<p>$sdate to $edate</p>";
-			echo "\t\t\t\t<p> $days_of_week's, $stime - $etime</p>";
-			echo "\t\t\t</li>";
-			echo "\t\t\t<li class='list-group-item'><p>Member Fee:   $$program->memberFee</p> <p>Non Member Fee:   $$program->nonMemberFee</p></li>";
-			echo "\t\t</ul>";
-			echo "\t\t<a href='/service/api.php?action=cancel&programID=$program->id' class='btn btn-block' style='background-color: red; color: white '>Cancel Registration</a>";
-			echo "\t</div>";
-			echo "</div>";
-	}
+function print_program($program)
+{
+    $sdate = $program->startDate->format("M, d Y");
+    $edate = $program->endDate->format("M, d Y");
+    $stime = $program->startTime->format('g:i A');
+    $etime = $program->endTime->format('g:i A');
+    $days_of_week = join("'s, ", $program->getDaysOfWeek());
+    echo "<div class='col-lg-4 col-md-6 mt-3 '>";
+    echo "\t<div class='card border-dark h-100' style='border-radius: 20px'>";
+    echo "\t\t<div class='card-body'>";
+    echo "\t\t\t<h5 class='card-title'>$program->name</h5>";
+    echo "\t\t\t<p class='card-text'>$program->shortDesc</p>";
+    echo "\t\t</div>";
+    echo "\t\t<ul class='list-group list-group-flush'>";
+    echo "\t\t\t<li class='list-group-item'>$program->location</li>";
+    echo "\t\t\t<li class='list-group-item'>";
+    echo "\t\t\t\t<p>$sdate to $edate</p>";
+    echo "\t\t\t\t<p> $days_of_week's, $stime - $etime</p>";
+    echo "\t\t\t</li>";
+    echo "\t\t\t<li class='list-group-item'><p>Member Fee:   $$program->memberFee</p> <p>Non Member Fee:   $$program->nonMemberFee</p></li>";
+    echo "\t\t</ul>";
+    echo "\t\t<a href='/service/api.php?action=cancel&programID=$program->id' class='btn btn-block' style='background-color: red; color: white '>Cancel Registration</a>";
+    echo "\t</div>";
+    echo "</div>";
+}
+
 ?>
 <h1>
-	<?php
-		echo "Welcome, $user->FirstName" . "!";
-	?>
+    <?php
+    echo "Welcome, $user->FirstName" . "!";
+    ?>
 </h1>
 <h2>Account Info</h2>
-            <div class="card shadow my-4" id="detail-card">
-                <div class="card-body">
-                    <h3 class="card-title" id="name">
-			<span id="lastName"><?php echo "$user->LastName"; ?></span>
-			<span id="firstName"><?php echo "$user->FirstName"; ?></span>
-                    </h3>
-                    <hr>
-                    <div class="card-text">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <small class="d-block mb-2"><strong>Email: </strong></small>
-                                <a id="email" href=""><?php echo "$user->Email"; ?></a>
-                            </div>
-                            <div class="col-lg-6">
-                                <small class="d-block mb-2"><strong>Membership Status: </strong></small>
-                                <span id="status"><?php echo "$user->MembershipStatus"; ?></span>
-                            </div>
-                        </div>
-</div>
-                    </div>
+<div class="card shadow my-4" id="detail-card">
+    <div class="card-body">
+        <h3 class="card-title" id="name">
+            <span id="lastName"><?php echo "$user->LastName"; ?></span>
+            <span id="firstName"><?php echo "$user->FirstName"; ?></span>
+        </h3>
+        <hr>
+        <div class="card-text">
+            <div class="row">
+                <div class="col-lg-6">
+                    <small class="d-block mb-2"><strong>Email: </strong></small>
+                    <a id="email" href=""><?php echo "$user->Email"; ?></a>
                 </div>
-	<?php if($DEBUG): 
-		echo "<tr>
+                <div class="col-lg-6">
+                    <small class="d-block mb-2"><strong>Membership Status: </strong></small>
+                    <span id="status"><?php echo "$user->MembershipStatus"; ?></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php if ($DEBUG):
+    echo "<tr>
 			<td>ID</td>
 			<td>$user->ID</td>
 		</tr>";
-	endif;
-	?>
-<br />
+endif;
+?>
+<br/>
 <button>UPDATE</button>
-<br />
+<br/>
 <h2>Registered Classes</h2>
 <div class="container">
-	<div class="row mt-3">
-		<?php 
-			$progs = Program::getParticipantProgram($user->ID);
-        	foreach ($progs as $obj) {
-        		print_program($obj);        
-        	}
-		?>
-	</div>
-	<br />
-	<br />
+    <div class="row mt-3">
+        <?php
+        $progs = Program::getParticipantProgram($user->ID ?? $user->userID);
+        foreach ($progs as $obj) {
+            print_program($obj);
+        }
+        ?>
+    </div>
+    <br/>
+    <br/>
 </div>
